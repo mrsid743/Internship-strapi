1f423b2e771b103caa1f70bb32ee65e16e83c85e
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-25 22:52:21 +0530

    Initial Terraform Strapi deployment setup

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,15 @@
-# outputs.tf
-# Defines the output values from our Terraform configuration.
+output "instance_id" {
+  description = "The ID of the EC2 instance."
+  value       = aws_instance.strapi_server.id
+}
 
 output "public_ip" {
-  description = "The public IP address of the Strapi EC2 instance."
+  description = "The public IP address of the EC2 instance."
   value       = aws_instance.strapi_server.public_ip
 }
 
 output "ssh_command" {
-  description = "Command to SSH into the EC2 instance."
-  value       = "ssh -i <path_to_your_private_key> ec2-user@${aws_instance.strapi_server.public_ip}"
+  description = "Command to SSH into the instance."
+  value       = "ssh -i strapi-mumbai-key.pem ec2-user@${aws_instance.strapi_server.public_ip}"
 }
+
