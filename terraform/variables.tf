4ad9304322246ae3b065ee2f595bc83e92d174d1
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-26 09:58:13 +0530

    Final fix for Terraform EC2 configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-# variables.tf
-# Input variables for the Terraform configuration.
-
 variable "aws_region" {
   description = "The AWS region to deploy resources in."
   type        = string
@@ -8,55 +5,11 @@ variable "aws_region" {
 }
 
 variable "ecr_repository_url" {
-  description = "The URL of the ECR repository (e.g., 123456789012.dkr.ecr.ap-south-1.amazonaws.com)."
+  description = "The URL of the ECR repository."
   type        = string
-}
-
-variable "ecr_repository_name" {
-  description = "The name of the ECR repository."
-  type        = string
-  default     = "siddhant-strapi"
 }
 
 variable "image_tag" {
-  description = "The Docker image tag to deploy."
+  description = "The tag of the Docker image to deploy."
   type        = string
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type to use."
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "aws_key_pair_name" {
-  description = "The EC2 key pair name to use for SSH access."
-  type        = string
-  default     = "strapi-mumbai-key"
-}
-
-variable "strapi_app_keys" {
-  description = "Comma-separated Strapi app keys for configuration."
-  type        = string
-}
-
-variable "strapi_api_token_salt" {
-  description = "Strapi API token salt."
-  type        = string
-}
-
-variable "strapi_admin_jwt_secret" {
-  description = "Strapi admin JWT secret."
-  type        = string
-}
-
-variable "strapi_jwt_secret" {
-  description = "Strapi JWT secret."
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "The public SSH key for accessing the EC2 instance."
-  type        = string
-  sensitive   = true
 }
