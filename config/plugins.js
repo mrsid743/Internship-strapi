8983c31ea062cb5e7a244b9ff7880267473de562
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-26 09:07:19 +0530

    Final fix for Terraform EC2 configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  // Example: upload plugin
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 1000000,
+      },
+    },
+  },
+});
